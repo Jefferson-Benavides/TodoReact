11c0387cc08e80e3bf845f98bd0495781bc3e489
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-12-29 09:24:21 -0500

    Renderizado de todos los componentes - nivel básico

--- todo-machine/src/App.js
+++ todo-machine/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoCounter } from './TodoCounter';
+import TodoItem from './TodoItem';
+import { TodoList } from './TodoList';
+import { TodoSearch } from './TodoSearch';
 
-function App(props) {
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de React', completed: false },
+  { text: 'Llorar con la llorona', completed: false }
+]
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Hola Jeffer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.children}
-        </a>
-      </header>
-    </div>
+    //etiqueta React.Fragment que equivale a un div
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
---
+++ todo-machine/src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = (props) => {
+    return (
+        <section>
+            {props.children}
+        </section>
+        
+    );
+};
+
+export { TodoList };
