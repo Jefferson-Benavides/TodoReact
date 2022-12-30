f0d63c062c87d986179d18052339b045287ce80c
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-12-30 10:02:44 -0500

    Cambios de estado y comentarios de uso antiguo con clases que extienden de Reac.Component

--- todo-machine/src/TodoSearch.js
+++ todo-machine/src/TodoSearch.js
@@ -3,17 +3,47 @@ import './TodoSearch.css'
 
 const TodoSearch = () => {
 
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
-        console.log(event.target.value)
+        console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
 
-    return (
+    return [
         <input 
         className='TodoSearch' 
         placeholder='Cebolla' 
+        value={searchValue}
         onChange={onSearchValueChange}
-        />
-    );
+        //Solo prueba de como cambiar un estado
+        // onChange={() => setState('Jefferson')}
+        />,
+        <p>
+            {searchValue}
+        </p>
+    ];
 };
 
 export { TodoSearch };
+
+
+// Forma de trabajo usando clases y no hooks
+// class Componente extends React.Component {
+//     constructor(){
+//         this.state = {
+//             patito: 'Juan'
+//         };
+//     }
+
+//     render(){
+//         return (
+//             <div
+//             onClick={() => this.setState('Enrique')}
+//             >
+//             {this.state.patito}
+//             </div>
+
+//         )
+//     }
+// }
